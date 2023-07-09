3dc60c6319c5b2cf9e4883d8f480bd0874971be5
Harshul Jain <harshulgangwal02@gmail.com>
2023-07-09 17:11:44 +0530

    Added Main Page

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -6,8 +6,8 @@ const Main = () => {
 
   return (
     <div className='container bg-dark'>
-      <h1 className='title text-light'>Quiz Application</h1>
-      <ol>
+      <h1 className='title text-light text-center p-3 my-3' style={{border: '5px solid #0cf36b'}}>Quiz Application</h1>
+      <ol className='text-light mt-5'>
         <li>You will be asked 10 questions one after another.</li>
         <li>10 points will be awarded for the correct answer.</li>
         <li>Each question has three options. These are multiple choice questions.</li>
@@ -15,12 +15,12 @@ const Main = () => {
         <li>The result will be declared at the end of the quiz.</li>
       </ol>
 
-      <form action="" id="form">
-        <input ref={inputRef} type="text" placeholder='Username*' />
+      <form action="" id="form" className='d-flex justify-content-center'>
+        <input ref={inputRef} type="text" placeholder='Username*' className='form-control w-50 mt-5'/>
       </form>
 
-      <div className="start">
-        <Link className='btn btn-primary' to='quiz'>Start Quiz </Link>
+      <div className="start d-flex justify-content-center">
+        <Link className='btn btn-info text-center mt-3' to={'quiz'}>Start Quiz </Link>
       </div>
     </div>
   )
