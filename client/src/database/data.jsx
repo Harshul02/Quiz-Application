d3e7277bdc703099d8f472bf89afa077f9493d55
Harshul Jain <harshulgangwal02@gmail.com>
2023-07-09 19:24:14 +0530

    Added Sample data

---
+++ client/src/database/data.jsx
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        question: "Javascript is an _____ language", 
+        options: [
+            'Object-Oriented',
+            'Object-Based',
+            'Procedural',
+        ]
+    },
+    {
+        id: 2,
+        question: "Following methods can be used to display data in some form using", 
+        options: [
+            'document.write()',
+            'console.log()',
+            'window.alert()',
+        ]
+    },
+    {
+        id: 3,
+        question: "What does the toString() method return?", 
+        options: [
+            'Return Object',
+            'Return String',
+            'Return Integer',
+        ]
+    },
+    {
+        id: 4,
+        question: "Which function is used to serialize an object into JSON string", 
+        options: [
+            'stringify()',
+            'parse()',
+            'convert()',
+        ]
+    },
+]
